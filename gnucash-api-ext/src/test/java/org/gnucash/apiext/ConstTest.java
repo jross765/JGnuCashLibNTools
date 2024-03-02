7e3ef274bf7af69219f902cc2df67c663aa1edb2
James Rossini <james.rossini@posteo.de>
2024-03-02 12:16:30 +0100

    - Module "Extensions": Test cases for DepotTransactionManager - Corrections in POM files

---
+++ gnucash-api-ext/src/test/java/org/gnucash/apiext/ConstTest.java
@@ -0,0 +1,15 @@
+package org.gnucash.apiext;
+
+public class ConstTest {
+
+    public static final String GCSH_FILENAME     = "test.gnucash";
+
+    public static final String GCSH_FILENAME_IN  = GCSH_FILENAME;
+
+    public static final String GCSH_FILENAME_OUT = "test_out.gnucash";
+    
+    // -----------------------------------------------------------------
+
+    public static final double DIFF_TOLERANCE = 0.005;
+
+}
